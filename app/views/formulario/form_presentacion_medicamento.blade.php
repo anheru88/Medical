3eebf63efbc86564a80e40d9abfcf79076f14ab0
hayderespit <hayderespit@hotmail.com>
2014-07-24 15:17:58 -0500

    Formulario presentación e medicamentos v1

---
+++ app/views/formulario/form_presentacion_medicamento.blade.php
@@ -0,0 +1,38 @@
+<div class="modal modal-flex fade" id="agregar_presentacion_medicamento" tabindex="-1" role="dialog" aria-labelledby="flexModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="flexModalLabel">Presentación Medicamento</h4>
+            </div>
+            <div class="modal-body">
+                <form action="" role="form">
+                 <div class="row">
+                    <div class="col-sm-6">
+                        <label for="">Codigo</label>
+                        <input type="text" class="form-control" id="" disabled>
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="">Unidad</label>
+                        <input type="text" class="form-control" id="" disabled>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <label for="">Nombre</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+            </form>
+
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-green">Guardar</button>
+        </div>
+    </div>
+    <!-- /.modal-content -->
+</div>
+<!-- /.modal-dialog -->
+</div>
