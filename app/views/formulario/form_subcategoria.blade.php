f1e410be2663d5651162c2047a452e675d1137dc
hayderespit <hayderespit@hotmail.com>
2014-07-24 14:47:15 -0500

    Formulario subcategoria de insumos v1

---
+++ app/views/formulario/form_subcategoria.blade.php
@@ -0,0 +1,43 @@
+<div class="modal modal-flex fade" id="agregar_subcategoria" tabindex="-1" role="dialog" aria-labelledby="flexModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="flexModalLabel">Datos Subcategoria</h4>
+            </div>
+            <div class="modal-body">
+                <form action="" role="form">
+                 <div class="row">
+                    <div class="col-sm-12">
+                        <label for="">Codigo</label>
+                        <input type="text" class="form-control" id="" disabled>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <label for="">Nombres</label>
+                        <input type="text" class="form-control" id="">
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <label for="">Categoria</label>
+                        <select class="form-control">
+                            <option>cat_1</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-green">Guardar</button>
+        </div>
+    </div>
+    <!-- /.modal-content -->
+</div>
+<!-- /.modal-dialog -->
+</div>
--- app/views/formulario/maestro_insumos.blade.php
+++ app/views/formulario/maestro_insumos.blade.php
@@ -27,10 +27,12 @@ Maestro de Insumos
 			</button>
 			@extends('formulario.form_categoria')
 			@parent
-			<button class="btn btn-info">
+			<button class="btn btn-info" data-toggle="modal" data-target="#agregar_subcategoria">
 				<i class="fa fa-plus-circle"></i>
-				Subcategoria
+				Subctaegoria
 			</button>
+			@extends('formulario.form_subcategoria')
+			@parent
 		</p>
 	</div>
 	<div class="col-sm-4 pull-right">
