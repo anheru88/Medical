3bf768f60f2e2d3d324536108356cb0bb9beab93
hayderespit <hayderespit@hotmail.com>
2014-07-29 12:16:44 -0500

    Formulario agregar sala

---
+++ app/views/formulario/form_sala.blade.php
@@ -0,0 +1,32 @@
+<div class="modal modal-flex fade" id="agregar_sala" tabindex="-1" role="dialog" aria-labelledby="flexModalLabel" aria-hidden="true">
+ <div class="modal-dialog">
+  <div class="modal-content">
+   <div class="modal-header">
+       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+       <h4 class="modal-title" id="flexModalLabel">Agregar Sala</h4>
+   </div>
+   <div class="modal-body">
+    <form action="" role="form">
+       <div class="row">
+         <div class="col-sm-12">
+           <label for="">Codigo</label>
+           <input type="text" class="form-control">
+       </div>
+       <div class="col-sm-12">
+           <label for="">Nombre Sala</label>
+           <input type="text" class="form-control">
+       </div>
+       <div class="col-sm-12">
+           <label for="">N° Camas</label>
+           <input type="text" class="form-control">
+       </div>
+   </div>
+</form>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+    <button type="button" class="btn btn-green">Guardar</button>
+</div>
+</div>
+</div>
+</div>
